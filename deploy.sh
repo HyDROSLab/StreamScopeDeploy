b2f048afcc9f408478dc9fe4849316dba155b88f
StreamscopePi <babetoduarte@gmail.com>
2025-02-07 11:58:56 -0600

    Added preemptive process kill to /dev/ttyUSB0 on deploy.sh to ensure the line is always available before attempting to start measuring.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+PORT="/dev/ttyUSB0"
+
+PID=$(lsof -t $PORT)
+[ -n "$PID" ] && sudo kill -9 $PID
+sudo chmod 666 $PORT
+
 /home/streamscope/StreamScopeDeploy/venv/bin/python /home/streamscope/StreamScopeDeploy/src/deploy.py
