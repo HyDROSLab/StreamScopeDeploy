274ceafd6f3a40a04567472a8240332e53751b2c
braden624 <bradenjames12@icloud.com>
2025-07-04 22:44:44 -0500

    Changes to deploy.py to accomodate firmware changes and new registers.

--- src/deploy.py
+++ src/deploy.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone
 import socket
 
 # Add project folder to path
-sys.path.insert(0, '/home/streamscope/StreamScopeDeploy/')
+sys.path.insert(0, '/home/braden/StreamScopeDeploy')
 
 class LidarMeasurement:
     def __init__(self):
@@ -46,40 +46,38 @@ class LidarSweep:
         self.right_bank = (0.0, 0.0)
         self.measurements.clear()
 
-
 UPDATE_TIME_REG = 0
 CURR_HOUR_REG = 1
 CURR_MIN_REG = 2
 CURR_DAY_REG = 3
 CURR_MONTH_REG = 4
 CURR_YEAR_REG = 5
-SWEEP_TYPE_REG = 6
+SWEEP_TYPE_REG = 6  # General Measurement = 1, Specific Measurement = 2, Configured Measurement = 3, 4 = Full Sweep
 NUM_MEASUREMENTS_REG = 7
 NUM_ANGLES_REG = 8
 NUM_SWEEPS_REG = 9
 MEASUREMENTS_READY_REG = 10
 SPECIFIC_ANGLES_START_REG = 11
 STAGE_MEASUREMENT_REG = 100
-AREA_MEASUREMENT_REG = 101
-STREAM_WIDTH_MEASUREMENT_REG = 102
-CENTROIDS_START_REG = 103
-LEFT_BANK_START_REG = 200
-RIGHT_BANK_START_REG = 202
-RAW_RESULTS_START_REG = 300
-EEPROM_INITIALIZE_REG = 5000
-EEPROM_NUM_ANGLES_REG = 5001
-EEPROM_NUM_MEASUREMENTS_REG = 5002
-EEPROM_ANGLES_START_REG = 5003
-ACCELEROMETER_AVAILABLE_REG = 5100
-STATE_REG = 5101
-RESET_REG = 5102
-DEBUG_REG = 5103
-TEMPERATURE_REG = 5104
+SERVO_CONTROL_TYPE_REG = 101
+ACCELEROMETER_AVAILABLE_REG = 102
+STATE_REG = 103
+RESET_REG = 104
+DEBUG_REG = 105
+TEMPERATURE_REG = 106
+CONTROL_TYPE_TWO_REG = 107  # 1 = Continuous, 2 = Single
+CONTROL_TYPE_FIVE_REG = 108  # 1 = Single/Slow, 2 = Single/Fast, 3 = Single/Auto, 4 = Continuous/Slow, 5 = Continuous/Fast, 6 = Continuous/Auto
+LASER_TIMEOUT_REG = 109
+RAW_RESULTS_START_REG = 110
+EEPROM_INITIALIZE_REG = 4000
+EEPROM_NUM_ANGLES_REG = 4001
+EEPROM_NUM_MEASUREMENTS_REG = 4002
+EEPROM_ANGLES_START_REG = 4003
 
 # Write to SWEEP_TYPE_REG with value 4. Don't write to NUM_ANGLES_REG, NUM_MEASUREMENTS_REG, NUM_SWEEPS_REG.
 # StreamScope will use it's internal values for these parameters. The variables below are for reading back data.
 # If full sweep needs to be changed, it must be done in the firmware.
-full_sweep = False
+full_sweep = True
 
 # Winter precip experiment
 angles = [angle + 32768 for angle in [-20, 0, 20]]
@@ -91,7 +89,7 @@ angles = [angle + 32768 for angle in [-20, 0, 20]]
 temperature = 9999
 
 if full_sweep:
-    num_angles = 91
+    num_angles = 101
     num_measurements = 30
 else:
     num_angles = len(angles)
@@ -99,7 +97,7 @@ else:
 
 num_sweeps = 1
 
-static_ip = '13.83.7.88'
+# static_ip = '13.83.7.88'
 
 log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'logs')
 os.makedirs(log_dir, exist_ok=True)
@@ -110,7 +108,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
-
 
 def get_lock(process_name):
     global lock_socket   # Without this our lock gets garbage collected
@@ -132,7 +129,7 @@ client.connect()
 def write_results(sweep):
 
     current_time = datetime.now(timezone.utc)
-    out_folder = "/home/streamscope/StreamScopeDeploy/data"
+    out_folder = "/home/braden/StreamScopeDeploy/data"
     file_name = f"{out_folder}/streamscope_log_{current_time.strftime('%Y%m%d_%H%M%S')}.txt"
 
     if not os.path.isfile(file_name):
@@ -152,30 +149,21 @@ def write_results(sweep):
             sdBuffer += "----------------------------------------\n\n"
             sdBuffer += f"Number of Angles: {num_angles}\n"
             sdBuffer += f"Number of Measurements at Each Angle: {num_measurements}\n"
-            sdBuffer += "Requested Angles (degrees): "
-            for i in range(num_angles):
-                sdBuffer += str(angles[i] - 32768)
-                if i != num_angles - 1:
-                    sdBuffer += ", "
+            sdBuffer += "Requested Angles (degrees): Full Sweep"
+            # for i in range(num_angles):
+                # sdBuffer += str(angles[i] - 32768)
+                # if i != num_angles - 1:
+                    # sdBuffer += ", "
             sdBuffer += "\n\n"
             sdBuffer += "----------------------------------------\n"
             sdBuffer += "         General Sweep Results          \n"
             sdBuffer += "----------------------------------------\n\n"
             sdBuffer += f"Sonar Distance (mm): {sweep.sonar_distance}\n"
-            sdBuffer += f"Left Bank Coordinates (mm): {sweep.left_bank}\n"
-            sdBuffer += f"Right Bank Coordinates (mm): {sweep.right_bank}\n"
-            sdBuffer += f"Stream Width (mm): {sweep.streamWidth}\n"
-            sdBuffer += f"Area (mm^2): {sweep.area}\n"
             sdBuffer += "Recorded Angles (degrees): "
             for i, measurement in enumerate(sweep.measurements):
                 sdBuffer += str(measurement.angle)
                 if i != len(sweep.measurements) - 1:
                     sdBuffer += ", "
-            sdBuffer += "\n"
-            sdBuffer += "Centroid Coordinates (corresponding to each recorded angle) (mm): \n"
-            for measurement in sweep.measurements:
-                for centroid in measurement.centroids:
-                    sdBuffer += f"(X: {centroid[0]}, Y: {centroid[1]})\n"
             sdBuffer += "\n"
             sdBuffer += "----------------------------------------\n"
             sdBuffer += "            Raw Measurements            \n"
@@ -194,7 +182,7 @@ def write_results(sweep):
         with open(file_name, 'rb') as f:
             files = {'file': (file_name, f, 'text/plain')}
             logging.info("Results written to file successfully.")
-
+    '''
     # Push output file to server via SFTP
     srv = pysftp.Connection(host=static_ip, username="pinpoint", password="Pinpoint2025#")
     try:
@@ -205,7 +193,7 @@ def write_results(sweep):
     except e:
         logging.error(e)
         logging.error("Results written to file but failed to send to server.")
-
+    '''
 def write_sweep_parameters():
     try:
         if client.read_holding_registers(DEBUG_REG, 1).registers[0] == 0:
@@ -216,17 +204,19 @@ def write_sweep_parameters():
         time.sleep(3)
         if full_sweep:
             logging.info("Writing full sweep.")
+            client.write_register(CONTROL_TYPE_FIVE_REG, 5);
+            client.write_register(LASER_TIMEOUT_REG, 1000)
+            client.write_register(SERVO_CONTROL_TYPE_REG, 2)
             client.write_register(SWEEP_TYPE_REG, 4)
         else:
-            logging.info("Writing number of angles.")
+            logging.info("Writing sweep parameters.")
+            client.write_register(CONTROL_TYPE_FIVE_REG, 5) # 4 = Continuous/Auto
+            client.write_register(LASER_TIMEOUT_REG, 1000)
             client.write_register(NUM_ANGLES_REG, num_angles)
-            logging.info("Writing number of measurements.")
             client.write_register(NUM_MEASUREMENTS_REG, num_measurements)
-            logging.info("Writing number of sweeps.")
             client.write_register(NUM_SWEEPS_REG, num_sweeps)
-            logging.info("Writing specific angles.")
             client.write_registers(SPECIFIC_ANGLES_START_REG, angles)
-            logging.info("Writing sweep type.")
+            client.write_register(SERVO_CONTROL_TYPE_REG, 2)
             client.write_register(SWEEP_TYPE_REG, 2)
     except Exception as e:
         logging.error(f"Error writing sweep parameters: {e}")
@@ -248,10 +238,6 @@ def deploy():
                 temperature = client.read_holding_registers(TEMPERATURE_REG, 1).registers[0] - 32768
                 sweep.sonar_distance = client.read_holding_registers(STAGE_MEASUREMENT_REG, 1).registers[0] - 32768
                 sweep.accelerometerAvailable = client.read_holding_registers(ACCELEROMETER_AVAILABLE_REG, 1).registers[0]
-                sweep.left_bank = (client.read_holding_registers(LEFT_BANK_START_REG, 1).registers[0] - 32768, client.read_holding_registers(LEFT_BANK_START_REG + 1, 1).registers[0] - 32768)
-                sweep.right_bank = (client.read_holding_registers(RIGHT_BANK_START_REG, 1).registers[0] - 32768, client.read_holding_registers(RIGHT_BANK_START_REG + 1, 1).registers[0] - 32768)
-                sweep.streamWidth = client.read_holding_registers(STREAM_WIDTH_MEASUREMENT_REG, 1).registers[0] - 32768
-                sweep.area = client.read_holding_registers(AREA_MEASUREMENT_REG, 1).registers[0] - 32768   
                 logging.info("Reading in raw measurements.")
                 raw_index = RAW_RESULTS_START_REG
                 for i in range(num_angles):
@@ -262,10 +248,6 @@ def deploy():
                         measurement.distances.append(client.read_holding_registers(raw_index, 1).registers[0] - 32768)
                         raw_index += 1
                     sweep.measurements.append(measurement)
-                centroids_index = CENTROIDS_START_REG
-                for i in range(num_angles):
-                    sweep.measurements[i].centroids.append((client.read_holding_registers(centroids_index, 1).registers[0] - 32768, client.read_holding_registers(centroids_index + 1, 1).registers[0] - 32768))
-                    centroids_index += 2
                 client.write_register(MEASUREMENTS_READY_REG, 0)
                 logging.info("Writing results to file.")
                 write_results(sweep)
